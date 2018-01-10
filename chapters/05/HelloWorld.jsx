39a858553385c252ed6ae8a55cc5180e9131f16e
Girma Nigusse <xgirma@gmail.com>
2018-01-10 00:27:56 -0800

    ch.05

--- chapters/05/HelloWorld.jsx
+++ chapters/05/HelloWorld.jsx
@@ -1,28 +1,11 @@
 class Hello extends React.Component {
-  constructor(props){
-    super(props);
-    this.oldSchoolFunction = this.oldSchoolFunction.bind(this);
-  }
   state = {
     last: 'Nigusse'
   };
 
-  oldSchoolFunction(){
-    console.log('old school function');
-    console.log('props', this.props);
-  };
-
-  newSchoolFunction = () => {
-    console.log('new school function');
-    console.log('props', this.props);
-  };
-
   render() {
-    return <div>
-      Hello Mr. {this.state.last}, {this.props.name} {this.props.middle}
-      <button onClick={this.oldSchoolFunction}>Old School</button>
-      <button onClick={this.newSchoolFunction}>New School</button>
-      </div>
+    console.log(this.state);
+    return <div>Hello Mr. {this.state.last}, {this.props.name} {this.props.middle}</div>
   }
 }
 
